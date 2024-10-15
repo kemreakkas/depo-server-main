ec223ab4fbb929d6a798d7d344aee2d17fb74b78
besteenurk <besteenurk@gmail.com>
2024-10-15 15:31:35 +0300

    calendar

--- api/index.js
+++ api/index.js
@@ -62,7 +62,7 @@ app.get('/api', (req, res) => {
 
 require('./routes/company.routes')(app)
 require('./routes/warehouse.routes')(app)
-require('./routes/sales.routes')(app)
+require('./routes/calendar.route')(app)
 // listen for requests
 app.listen( 3001, () => {
     console.log("Sunucu 3000 portunda hizmet vermekte.");
